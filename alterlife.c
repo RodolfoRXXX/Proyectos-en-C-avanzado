100d1f44926184b54677b3acba81a692dedd2d87
Rodolfo <rodolfo.singular@gmail.com>
2025-02-24 16:51:56 -0300

    agrego código para modificar estados de memoria de un juego

---
+++ alterlife.c
@@ -0,0 +1,22 @@
+/*
+    Código que modifica el valor de una variable en memoria de un juego
+
+    1 - Programa objetivo : Simulación de juego
+    2 - Escaner de memoria : busca direcciones de memoria de un programa, puede utilizarse para esto el cheat Engine
+    3 - Dirección de memoria estática y dirección de memoria dinámica
+
+    Objetivos:
+            - Obtener dirección de memoria de la variable que almacena la cantidad de vidas en mi juego
+            - Modificar el valor de esa variable
+
+*/
+
+#include <stdio.h>
+#include <windows.h>
+
+int main()
+{
+
+
+    return 0;
+}
