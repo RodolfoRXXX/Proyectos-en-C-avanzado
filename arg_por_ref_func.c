3e3d43f275c1b0068e873e41098fbcd23c7d395d
Rodolfo <rodolfo.singular@gmail.com>
2025-07-17 16:46:20 -0300

    update

--- arg_por_ref_func.c
+++ arg_por_ref_func.c
@@ -7,25 +7,42 @@
 
 #include <stdio.h>
 
+// defino una nueva estructura de dato
 typedef struct {
     int age;
+    float high;
 } Persona;
 
+// prototipos
 void sumaEdad(Persona * p);
+void sumaAltura(Persona * p);
 
 int main(void)
 {
     Persona p;
     p.age = 23;
+    p.high = 1.78;
 
+    // pasamos a la función la ubicación de la variable en la memoria
     sumaEdad(&p);
+    sumaAltura(&p);
 
-    printf("La edad es %d", p.age);
+    printf("La edad es %d\n", p.age);
+    printf("La altura es %.2f\n", p.high);
 
     return 0;
 }
 
+// definición de funciones
+//  esta función recibe la dirección de la variable en un puntero definido como argumento
 void sumaEdad(Persona * p)
 {
+    // modifica así la variable original accediendo a su dirección en memoria
     (*p).age++;
+}
+
+// accede a la dirección de la variable y la modifica
+void sumaAltura(Persona * p)
+{
+    p->high = 1.85;
 }
