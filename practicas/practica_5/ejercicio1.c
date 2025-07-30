d437f7595e93d6191ad8aac405d17a3267d1e967
Rodolfo <rodolfo.singular@gmail.com>
2025-07-30 16:53:12 -0300

    update

---
+++ practicas/practica_5/ejercicio1.c
@@ -0,0 +1,20 @@
+/*
+    Ejercicio 1
+
+    Escribir un programa en C que escriba los números comprendidos entre 1 y 1000. El
+    programa escribirá en la pantalla los números en grupos de 20, solicitando al usuario si
+    quiere o no continuar visualizando el siguiente grupo de números. Generalizar el
+    programa para que escriba los números comprendidos entre dos valores que introduzca
+    el usuario, y sea éste también quien decida el tamaño del grupo a visualizar por
+    pantalla
+
+*/
+
+#include <stdio.h>
+
+int main(void)
+{
+
+
+    return 0;
+}
